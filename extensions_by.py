4bd67e2ba563f83576bda9706eb5ef068a51eb7e
galardodis <132752032+galardodis@users.noreply.github.com>
2023-12-12 12:35:36 +0300

    Add files via upload

---
+++ extensions_by.py
@@ -0,0 +1,52 @@
+import requests
+import json
+from get_currencie_by import get_currencie
+
+currencie = get_currencie()
+
+
+class APIException(Exception):
+  pass
+
+
+class CryptoConverter:
+
+  @staticmethod
+  def get_price(quote=str, base=str, amount=str):
+    try:
+      amount = float(amount)
+    except ValueError:
+      raise APIException(f'Мне кажется "{amount}" - это не цифра, '
+                         f'Попробуй ешё раз\n\nПример ввода: 10 USD BYN')
+
+    if quote == base:
+      raise APIException(f'Невозможно перевести одинаковае валюты {quote}')
+
+    if quote == 'BYN' or base == 'BYN':
+      if quote == 'BYN':
+        if base not in currencie.keys():
+          raise APIException(f'Я не знаю волюту "{base}", '
+                             f'Попробуй ешё раз\n\nПример ввода: 10 USD BYN')
+        else:
+          total_base = round(
+              (currencie[base][0] / currencie[base][2]) * amount, 2)
+      else:
+        if quote not in currencie.keys():
+          raise APIException(f'Я не знаю волюту "{quote}", '
+                             f'Попробуй ешё раз\n\nПример ввода: 10 USD BYN')
+        else:
+          total_base = round(
+              (currencie[quote][2] / currencie[quote][0]) * amount, 2)
+    else:
+      if quote not in currencie.keys():
+        raise APIException(f'Я не знаю волюту "{quote}", '
+                           f'Попробуй ешё раз\n\nПример ввода: 10 USD BYN')
+
+      if base not in currencie.keys():
+        raise APIException(f'Я не знаю волюту "{base}", '
+                           f'Попробуй ешё раз\n\nПример ввода: 10 USD BYN')
+
+      total_base = round(
+          ((currencie[quote][2] / currencie[quote][0]) * amount) /
+          (currencie[base][2] / currencie[base][0]), 2)
+    return total_base
---
+++ get_currencie_by.py
@@ -0,0 +1,21 @@
+import requests
+import json
+import text
+
+a = json.loads(requests.get(text.url_nbrb_cur).content)
+b = json.loads(requests.get(text.url_nbrb_all).content)
+
+
+def get_currencie():
+    currencie = {}
+    for i in a:
+        currencie[i['Cur_Abbreviation']] = [i['Cur_Scale'], i['Cur_Name'], i['Cur_OfficialRate']]
+        for j in b:
+            if j['Cur_Abbreviation'] == i['Cur_Abbreviation']:
+                currencie[j['Cur_Abbreviation']].append(j['Cur_Name'])
+                currencie[j['Cur_Abbreviation']].append('BYN')
+                break
+    sorted_tuples = sorted(currencie.items(), key=lambda item: item[1][3])
+    currencie = {k: v for k, v in sorted_tuples}
+    return currencie
+
