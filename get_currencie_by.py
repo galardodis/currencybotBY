5a935c40ee783e2e81b5c098ce439854e2471da2
galardodis <ilyla_dis@mail.ru>
2023-12-12 18:08:02 +0300

    Upgate 12.12.23

--- get_currencie_by.py
+++ get_currencie_by.py
@@ -5,7 +5,6 @@ import text
 a = json.loads(requests.get(text.url_nbrb_cur).content)
 b = json.loads(requests.get(text.url_nbrb_all).content)
 
-
 def get_currencie():
     currencie = {}
     for i in a:
@@ -15,7 +14,8 @@ def get_currencie():
                 currencie[j['Cur_Abbreviation']].append(j['Cur_Name'])
                 currencie[j['Cur_Abbreviation']].append('BYN')
                 break
+
+    currencie['BYN'] = [1, 'Беларусский рубль', 1, 'Беларусский рубль', 'BYN']
     sorted_tuples = sorted(currencie.items(), key=lambda item: item[1][3])
     currencie = {k: v for k, v in sorted_tuples}
     return currencie
-
--- main.py
+++ main.py
@@ -3,57 +3,33 @@ import time
 from telebot import types
 from tokens import TELE_TOKEN
 from extensions_by import CryptoConverter, APIException, currencie
-from background import keep_alive  #постоянный онлайн
+from background import keep_alive  # постоянный онлайн
 
 # import bot_app_world
 
 bot = telebot.TeleBot(TELE_TOKEN)
 
+ls = []
+
 
 @bot.message_handler(commands=['start'])
 def start(message: telebot.types.Message):
-    # markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    # btn1 = types.KeyboardButton('/Мировые_курсы_валют')
-    # btn2 = types.KeyboardButton('/Курсы_валют_Беларусь')
-    # markup.add(btn1, btn2)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton('Курсы валют')
+    btn2 = types.KeyboardButton('Конвертация')
+    markup.add(btn1, btn2)
     text = 'Привет! Я Бот-Конвертер валют и я могу:  \n\n' \
-           '- Показать список доступных валют и их кодов через команду: /values' \
-           '\n\n- Показать курсы валют на сегодня по отношению к белорусскрму рублю через команду /course' \
-           '\n\n- Вывести конвертацию валюты через команду:\n' \
-           '<Количество переводимой валюты>  ' \
-           '<Код валюты для перевода>  ' \
-           '<Код валюты в кототую нужно перевести>  ' \
-           '\n\nПример ввода: 10 USD BYN' \
-           '\n\n' \
-           'Напомнить, что я могу через команду: /help'
-    bot.send_message(message.chat.id, text)
-    # bot.send_message(message.chat.id, text, reply_markup=markup)
-
-
-# @bot.message_handler(commands=['Мировые_курсы_валют'])
-# def start(message):
-#     bot.send_message(message.chat.id,
-#                      text="Отправляемся путешествовать по миру!".format(
-#                          message.from_user))
-#
-#
-# @bot.message_handler(commands=['Курсы_валют_Беларусь'])
-# def start(message):
-#     bot.send_message(message.chat.id,
-#                      text="Мы в беларуси!".format(
-#                          message.from_user))
+           '- Показать курсы валют по отношению к белорусскому рублю (Кнопка "Курсы валют")' \
+           '\n\n- Конвертировать валюты (кнопка "Конвертация")' \
+           '\n\nНапомнить, что я могу через команду: /help'
+    bot.send_message(message.chat.id, text, reply_markup=markup)
 
 
 @bot.message_handler(commands=['help'])
 def helpp(message: telebot.types.Message):
     text = 'Я могу:  \n\n' \
-           '- Показать список доступных валют и их кодов через команду: /values' \
-           '\n\n- Показать курсы валют на сегодня по отношению к белорусскрму рублю через команду /course' \
-           '\n\n- Вывести конвертацию валюты через команду:\n' \
-           '<Количество переводимой валюты>  ' \
-           '<Код валюты для перевода>  ' \
-           '<Код валюты в кототую нужно перевести>  ' \
-           '\n\nПример ввода: 10 USD BYN' \
+           '- Показать курсы валют по отношению к белорусскому рублю (Кнопка "Курсы валют")' \
+           '\n\n- Конвертировать валюты (кнопка "Конвертация")' \
            '\n\n' \
            'Напомнить, что я могу через команду: /help'
     bot.send_message(message.chat.id, text)
@@ -63,48 +39,56 @@ def helpp(message: telebot.types.Message):
 def course(message: telebot.types.Message):
     text = 'Курсы валют на сегодня:'
     for key in currencie:
-        text = '\n'.join((text, f'{currencie[key][0]} {currencie[key][1]} ({key}) - {currencie[key][2]} BYN'))
+        text = '\n'.join((text, f'{currencie[key][0]} {currencie[key][1]} ({key}) = {currencie[key][2]} BYN'))
     text += f'\n\nПо курсу НБ РБ на {time.strftime("%d %b %Y %H:%M:%S")}'
-    text += '\n\nCписок доступных валют и их кодов через команду: /values'
-    text += '\n\nНапомнить, что я могу через команду: /help'
-    bot.send_message(message.chat.id, text)
-
-
-@bot.message_handler(commands=['values'])
-def values(message: telebot.types.Message):
-    text = 'Доступные валюты:'
-    for key in currencie:
-        text = '\n'.join((text, f'{currencie[key][3]} ({key})'))
-    text += '\n\nКурсы валют на сегодня по отношению к белорусскрму: /course'
     text += '\n\nНапомнить, что я могу через команду: /help'
     bot.send_message(message.chat.id, text)
 
 
 @bot.message_handler(content_types=['text', ])
 def convert(message: telebot.types.Message):
-    try:
-        values = message.text.split(' ')
+    if message.text == 'Курсы валют':
+        text = 'Курсы валют на сегодня:'
+        for key in currencie:
+            text = '\n'.join((text, f'{currencie[key][0]} {currencie[key][1]} ({key}) = {currencie[key][2]} BYN'))
+        text += f'\n\nПо курсу НБ РБ на {time.strftime("%d %b %Y %H:%M:%S")}'
+        text += '\n\nНапомнить, что я могу через команду: /help'
+        bot.send_message(message.chat.id, text)
 
-        if len(values) > 3:
-            raise APIException('Слишком много параметров, '
-                               'Попробуй ешё раз\n\nПример ввода: 10 USD BYN')
+    elif message.text == 'Конвертация':
+        bot.send_message(message.chat.id, 'Введите сумму конвертации')
 
-        elif len(values) < 3:
-            raise APIException('Слишком мало параметров, '
-                               'Попробуй ешё раз\n\nПример ввода: 10 USD BYN')
-
-        amount, quote, base = values[0], values[1].upper(), values[2].upper()
-        total_base = CryptoConverter.get_price(quote, base, amount)
-
-    except APIException as e:
-        bot.reply_to(message, f'Ты ошибся с вводом\n{e}')
-
-    except Exception as e:
-        bot.reply_to(message, f'Не удалось обработать команду \n{e}')
+    elif message.text.isdigit() and len(ls) == 0:
+        ls.append(int(message.text))
+        markup = types.InlineKeyboardMarkup()
+        for key in currencie:
+            markup.add(types.InlineKeyboardButton(
+                f'{currencie[key][3]} ({key})', callback_data=key))
+        bot.send_message(message.chat.id, 'Доступные валюты:', reply_markup=markup)
+        bot.send_message(message.chat.id, 'Выберите валюту конвертации из представленных выше')
     else:
+        ls.clear()
+        bot.send_message(message.chat.id, 'Введите сумму конвертации')
+
+
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+    if len(ls) == 0:
+        bot.send_message(callback.message.chat.id, 'Введате сумму конвертации')
+    elif len(ls) < 2:
+        ls.append(callback.data)
+        bot.send_message(callback.message.chat.id, f'Вы выбрали {currencie[callback.data][3]} ({callback.data})')
+        bot.send_message(callback.message.chat.id, f'Выберите валюту в которую конвертируем из представленных выше')
+    elif len(ls) == 2:
+        bot.send_message(callback.message.chat.id, f'Вы выбрали {currencie[callback.data][3]} ({callback.data})')
+        ls.append(callback.data)
+
+        amount, quote, base = ls
+        total_base = CryptoConverter.get_price(quote, base, amount)
         text = f'{amount} {quote} = {total_base} {base}\n' \
                f'По курсу НБ РБ на {time.strftime("%d %b %Y %H:%M:%S")}'
-        bot.send_message(message.chat.id, text)
+        bot.send_message(callback.message.chat.id, text)
+        ls.clear()
 
 
 # keep_alive() #постоянный онлайн
